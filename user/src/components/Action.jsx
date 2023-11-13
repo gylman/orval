588192568947d03285f0237e4a8a0111ef1ae814
gylman <gylmanbm@gmail.com>
2023-11-13 19:12:33 +0900

    separate into Action component

---
+++ user/src/components/Action.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import PlayPause from './PlayPause';
+import axios from 'axios';
+
+const Main = styled.div`
+  background: #4661e6;
+  border-radius: 3px;
+  padding: 5px;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Text = styled.span`
+  color: #fff;
+`;
+const Action = ({ children, onClick, isRunning }) => {
+  return (
+    <Main>
+      <Text>{children}</Text>{' '}
+      <PlayPause onClick={onClick} isPlaying={isRunning} />
+    </Main>
+  );
+};
+
+export default Action;
--- user/src/components/ActionMenu.jsx
+++ user/src/components/ActionMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { styled } from 'styled-components';
 import PlayPause from './PlayPause';
 import axios from 'axios';
+import Action from './Action';
 
 const Main = styled.div`
   background: #f2f2f2;
@@ -21,69 +22,34 @@ const Actions = styled.div`
   flex-direction: column;
   gap: 6px;
 `;
-const Action = styled.div`
-  background: #4661e6;
-  border-radius: 3px;
-  padding: 5px;
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-`;
 
-const Text = styled.span`
-  color: #fff;
-`;
 const Title = styled.p`
   color: #fff;
   font-size: 24px;
 `;
 
 const ActionMenu = () => {
-  const sendTx = () => {};
   return (
     <Main>
       <Actions>
         <Title>User</Title>
-        <Action>
-          <Text>Send tx</Text> <PlayPause onClick={sendTx} isPlaying={false} />{' '}
-        </Action>
-        <Action>
-          <Text>Claim</Text> <PlayPause isPlaying={false} />
-        </Action>
+        <Action>Send tx</Action>
+        <Action>Claim</Action>
       </Actions>
       <Actions>
         <Title>Sequencer</Title>
-        <Action>
-          <Text>Order tx</Text>
-          <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Sign</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Send to user</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Send hashes to L2</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Send tx list to L2</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Store hashes to L1</Text> <PlayPause isPlaying={false} />
-        </Action>
+        <Action>Order tx</Action>
+        <Action>Sign</Action>
+        <Action>Send to user</Action>
+        <Action>Send hashes to L2</Action>
+        <Action>Send tx list to L2</Action>
+        <Action>Store hashes to L1</Action>
       </Actions>
       <Actions>
         <Title>L2</Title>
-        <Action>
-          <Text>Request block</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Sign hashchain</Text> <PlayPause isPlaying={false} />
-        </Action>
-        <Action>
-          <Text>Send to sequencer</Text> <PlayPause isPlaying={false} />
-        </Action>
+        <Action>Request block</Action>
+        <Action>Sign hashchain</Action>
+        <Action>Send to sequencer</Action>
       </Actions>
     </Main>
   );
